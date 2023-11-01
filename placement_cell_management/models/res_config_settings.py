dfeec516968644e3e46972b28ba9c2774cac7196
rohan_movaliya <ROM@BAD.com>
2023-11-01 11:57:26 +0530

    [WIP][T00468]placement-cell-management:set pre-commit file

--- placement_cell_management/models/res_config_settings.py
+++ placement_cell_management/models/res_config_settings.py
@@ -6,12 +6,7 @@ class ResConfigSettings(models.TransientModel):
 
     # T00468 addded fields
     year_of_graduation = fields.Char(
-        default="2023-2024",
         config_parameter="placement_cell_management.year_of_graduation",
     )
-    attendence = fields.Float(
-        default="90.00", config_parameter="placement_cell_management.attendence"
-    )
-    cgpa = fields.Float(
-        default="7.00", config_parameter="placement_cell_management.cgpa"
-    )
+    attendence = fields.Float(config_parameter="placement_cell_management.attendence")
+    cgpa = fields.Float(config_parameter="placement_cell_management.cgpa")
